7290a2fa6cf8b6ec4009946c8bc7ca828ac85970
Chukwuma Nwaugha <c.nwaugha@gmail.com>
2019-09-25 11:39:39 +0100

    install pod dependencies

---
+++ ios/Pods/Target Support Files/Pods-EventsMag-tvOSTests/Pods-EventsMag-tvOSTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_EventsMag_tvOSTests : NSObject
+@end
+@implementation PodsDummy_Pods_EventsMag_tvOSTests
+@end
---
+++ ios/Pods/Target Support Files/Pods-EventsMag/Pods-EventsMag-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_EventsMag : NSObject
+@end
+@implementation PodsDummy_Pods_EventsMag
+@end
